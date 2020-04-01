7556cd4bb36bc99103d520e5062f3cf6988018d2
svendp1988 <56538162+svendp1988@users.noreply.github.com>
2020-04-01 21:07:16 +0200

    adding tests

---
+++ service/src/test/java/com/switchfully/service/user/UserDtoTest.java
@@ -0,0 +1,25 @@
+package com.switchfully.service.user;
+
+import com.switchfully.domain.user.Address;
+import org.junit.jupiter.api.Test;
+
+import static com.switchfully.domain.user.builders.UserBuilder.userBuilder;
+import static com.switchfully.service.testbuilders.TestAddressBuilder.testAddressBuilder;
+import static com.switchfully.service.testbuilders.TestUserBuilder.testUserBuilder;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserDtoTest {
+    @Test
+    void testEquals() {
+        UserDto userDto1 = new UserDto(testUserBuilder().buildCustomer());
+        Address address = testAddressBuilder().build();
+        UserDto userDto2 = new UserDto(userBuilder()
+                .withFirstName("firstName")
+                .withLastName("lastName")
+                .withEmail("email")
+                .withPassword("password")
+                .withAddress(address)
+                .buildCustomer());
+        assertTrue(userDto1.equals(userDto2));
+    }
+}
