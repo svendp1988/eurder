c133899e7a9366c3ada0a923f42c3be255dc833f
Sven De Potter <sven.depotter@hotmail.com>
2020-04-03 16:51:39 +0200

    Mockito testing

--- api/src/test/java/com/switchfully/api/endpoints/UserControllerTest.java
+++ api/src/test/java/com/switchfully/api/endpoints/UserControllerTest.java
@@ -4,35 +4,28 @@ import com.switchfully.service.user.UserService;
 import com.switchfully.service.user.dto.CreateUserDto;
 import com.switchfully.service.user.dto.UserDto;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static com.switchfully.api.testbuilders.TestUserDtoBuilder.testUserDtoBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
     CreateUserDto newCustomer = testUserDtoBuilder().buildCreateUserDto();
     UserDto userDto = testUserDtoBuilder().buildCustomerDto();
-//
-//    @Mock
-//    UserRepository userRepository = new UserRepository();
-//    @Mock
-//    UserMapper userMapper = new UserMapper(new AddressMapper(), new UserRoleMapper());
-//    = new UserService(new UserRepository(), new UserMapper(new AddressMapper(), new UserRoleMapper()));
 
-//    UserService userService = Mockito.mock(UserService.class);
+    @Mock
+    UserService userService;
+    @InjectMocks
     UserController userController;
 
     @Test
     void registeringANewCustomer_returnsDtoHoldingTheSameData() {
-        UserService userService = Mockito.mock(UserService.class);
-        UserController userController = new UserController(userService);
         when(userService.registerAsCustomer(newCustomer)).thenReturn(userDto);
         assertThat(userController.register(newCustomer)).isEqualTo(userDto);
     }
